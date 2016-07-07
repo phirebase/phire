3c3684b478358eeb0e2d6726e160b8fe4ae22c22
Myke Howells <myke@mykeand.co>
2016-07-07 16:58:17 +0100

    Added default routes file.

---
+++ app/Http/routes.php
@@ -0,0 +1,16 @@
+<?php
+
+/*------------------------------------------------------------
+ | Routes
+ |------------------------------------------------------------
+ |
+ | Define your routes in this file. Currently supported are,
+ | GET, POST, PUT, DELETE, OPTION, CONNECT.
+ |
+ */
+
+Route::get( '/', function () {
+
+	return;
+
+} );
