6fe796d52e304f6ef33d01a7efd1cc710b2692cf
Myke Meynell <myke@mykemeynell.com>
2016-12-15 22:03:02 +0000

    Added bootstrap call to index to bootstrap application

--- public/index.php
+++ public/index.php
@@ -20,3 +20,26 @@ require __DIR__ . '/../bootstrap/bootstrap.php';
 |---------------------------------------------------------------------------*/
 
 $app = require __DIR__ . '/../bootstrap/application.php';
+
+/*----------------------------------------------------------------------------
+| Launch the Application
+|-----------------------------------------------------------------------------
+|
+| Get the incoming request, handle it, and return a response from within the
+| application runtime.
+|
+|---------------------------------------------------------------------------*/
+
+$kernel = $app->make(\Phire\Support\Contracts\Http\Kernel::class);
+
+/*----------------------------------------------------------------------------
+| Bootstrap
+|-----------------------------------------------------------------------------
+|
+| Go over the bootstrap classes and add handle them appropriately
+| within the application container.
+|
+|---------------------------------------------------------------------------*/
+
+$kernel->bootstrap();
+
