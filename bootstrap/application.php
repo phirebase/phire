71556fab92771c8fbeb5284b20333aa3c1ceb184
Myke Meynell <myke@mykemeynell.com>
2016-12-15 22:00:25 +0000

    Changed Kernel class singleton relationship to reference class within the app rather than support or framework

--- bootstrap/application.php
+++ bootstrap/application.php
@@ -12,4 +12,19 @@ $app = new \Phire\Support\Factory\Application(
 	realpath(__DIR__ . '/../')
 );
 
+/*------------------------------------------------------------
+ | Load Bindings into Container
+ |------------------------------------------------------------
+ |
+ | Here we can use the bind() and singleton() container methods
+ | to add bindings into the application, we do this here so
+ | that they are available throughout the application.
+ |
+ */
+
+$app->singleton(
+    \Phire\Support\Contracts\Http\Kernel::class,
+    \App\Http\Kernel::class
+);
+
 return $app;
