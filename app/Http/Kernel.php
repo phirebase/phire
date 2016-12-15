102b52f15259fe2c1397f6a4c6b817c58d6dfef7
Myke Meynell <myke@mykemeynell.com>
2016-12-15 22:02:28 +0000

    New file for internal Kernel to application to extend out of vendor

---
+++ app/Http/Kernel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http;
+
+use Phire\Framework\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel {
+
+    /**
+     * Classes that are used to bootstrap the application.
+     *
+     * @var array
+     */
+    protected $bootstrap = [
+        \Phire\Framework\Bootstrap\LoadEnvironment::class,
+        \Phire\Framework\Bootstrap\LoadEnvironment::class,
+    ];
+
+}
