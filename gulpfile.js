4cd24757a6758e738f9dd6217b90e80ad3fdf295
Myke Meynell <myke@mykemeynell.com>
2016-12-17 22:15:59 +0000

    Updated package.json and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,21 @@
-var gulp = require('gulp'),
-    plumber = require('gulp-plumber'),
-    rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin'),
-    cache = require('gulp-cache');
-var minifycss = require('gulp-minify-css');
+'use strict';
+
+var dir = {
+    sass: {
+        src: './resources/assets/sass/**/*.scss',
+        dest: './public/assets/css'
+    }
+};
+
+var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('images', function(){
-    gulp.src('resources/images/**/*')
-        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-        .pipe(gulp.dest('public/assets/images/'));
+gulp.task('sass', function () {
+    return gulp.src(dir.sass.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(dir.sass.dest));
 });
 
-gulp.task('styles', function(){
-    gulp.src(['resources/assets/sass/**/*.scss'])
-        .pipe(plumber({
-            errorHandler: function (error) {
-                console.log(error.message);
-                this.emit('end');
-            }}))
-        .pipe(sass())
-        .pipe(autoprefixer('last 2 versions'))
-        .pipe(gulp.dest('public/assets/css/'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(minifycss())
-        .pipe(gulp.dest('public/assets/css/'))
-});
-
-gulp.task('scripts', function(){
-    return gulp.src('resources/assets/sass/**/*.js')
-        .pipe(plumber({
-            errorHandler: function (error) {
-                console.log(error.message);
-                this.emit('end');
-            }}))
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('public/assets/js/'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest('public/assets/js/'))
-});
-
-gulp.task('default', function(){
-    gulp.watch("resources/assets/sass/**/*.scss", ['styles']);
-    gulp.watch("resources/assets/sass/**/*.js", ['scripts']);
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
 });
