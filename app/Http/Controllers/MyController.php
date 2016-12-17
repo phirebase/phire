cf46c832e60f0dd3ec5bfd8f0c575f5c11f83b1f
Myke Meynell <myke@mykemeynell.com>
2016-12-17 12:37:49 +0000

    Added class comment to demo controller and index method

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * Class MyController.
+ *
+ * @package App\Http\Controllers
+ */
 class MyController {
 
     /**
+     * Index method for example controller.
+     *
      * @return mixed
      */
     public function index() {
