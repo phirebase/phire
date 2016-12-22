b9c6d3a870b29bec719f9341ed21fe7b8c8709ed
Myke Meynell <myke@mykemeynell.com>
2016-12-22 20:32:07 +0000

    Changed comment formatting so its consistant

--- public/index.php
+++ public/index.php
@@ -17,18 +17,18 @@ require __DIR__ . '/../bootstrap/bootstrap.php';
 |
 | Get everything ready for the application to run.
 |
-|---------------------------------------------------------------------------*/
+*/
 
 $app = require __DIR__ . '/../bootstrap/application.php';
 
 /*----------------------------------------------------------------------------
-| Launch the Application
+| Create the application kernel.
 |-----------------------------------------------------------------------------
 |
-| Get the incoming request, handle it, and return a response from within the
-| application runtime.
+| Create an instance of the Kernel to handle incomming requests, and allow
+| bootstrapping of application.
 |
-|---------------------------------------------------------------------------*/
+*/
 
 $kernel = $app->make(\Phire\Support\Contracts\Http\Kernel::class);
 
@@ -39,7 +39,7 @@ $kernel = $app->make(\Phire\Support\Contracts\Http\Kernel::class);
 | Go over the bootstrap classes and add handle them appropriately
 | within the application container.
 |
-|---------------------------------------------------------------------------*/
+*/
 
 $kernel->bootstrap();
 
@@ -49,7 +49,7 @@ $kernel->bootstrap();
 |
 | Get the current request, translate into a response and send the response.
 |
-|---------------------------------------------------------------------------*/
+*/
 
 $request = \Phire\Http\Request::gather();
 
